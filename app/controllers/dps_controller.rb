b3b8d852be90f3a6c488cb59d9eb19b4a7b11dc5
paqpac <94620920+paqpac@users.noreply.github.com>
2022-04-16 10:22:01 +0900

    jquery実装止め

--- app/controllers/dps_controller.rb
+++ app/controllers/dps_controller.rb
@@ -1,5 +1,7 @@
 class DpsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :params_id, except: [:index, :new, :create, :search]  
+  before_action :move_to_index, except: [:index,:new, :create, :show, :search]
 
   def index
     @dps = Dp.all.order("created_at DESC")
@@ -20,15 +22,12 @@ class DpsController < ApplicationController
   end
 
   def show
-    @dp = Dp.find(params[:id])
   end
 
   def edit
-    @dp  = Dp.find(params[:id])
   end
 
   def update
-    @dp = Dp.find(params[:id])
     if @dp.update(dp_params)
       redirect_to dp_path
     else
@@ -37,7 +36,6 @@ class DpsController < ApplicationController
   end
 
   def destroy
-    @dp = Dp.find(params[:id])
     @dp.destroy!
   end
 
@@ -48,6 +46,16 @@ class DpsController < ApplicationController
   private
 
   def dp_params
-    params.require(:dp).permit(:name, :product_number, :jan_code, :category_id, :content, :image, :client, :source_id, :return_id, :confirm_date, :occurrence_date, :lotnumber, :summary).merge(user_id: current_user.id)
+    params.require(:dp).permit(:name, :product_number, :jan_code, :category_id, :image, :client, :source_id, :return_id, :confirm_date, :occurrence_date, :lotnumber, :summary).merge(user_id: current_user.id)
+  end
+
+  def params_id
+    @dp = Dp.find(params[:id])
+  end
+  
+  def move_to_index
+    unless user_signed_in? && current_user.id == @dp.user_id
+      redirect_to root_path
+    end
   end
 end
